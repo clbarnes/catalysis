567aff78c3620c7fd5e85214a9ac672b4efe20c4
Chris Barnes <cbarnes@mrc-lmb.cam.ac.uk>
2022-09-23 15:07:18 +0100

    Add extras to setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,17 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-with open(path.join(here, "requirements.txt"), "r") as f:
-    requirements = f.read().splitlines()
+
+def parse_requirements(extra=None):
+    if extra is None:
+        suffix = ""
+    else:
+        suffix = "-" + extra
+    with open(path.join(here, "requirements" + suffix + ".txt")) as f:
+        return f.read().splitlines()
+
+install_requires = parse_requirements()
+extras_require = {n: parse_requirements(n) for n in ["dev", "notebooks"]}
 
 setup(
     name="catalysis",
@@ -19,7 +28,8 @@ setup(
     author="Casey Schneider-Mizell",
     author_email="caseysm@gmail.com",
     packages=["catalysis"],
-    install_requires=requirements,
+    install_requires=install_requires,
+    extras_require=extras_require,
     include_package_data=True,
     data_files=[
         (
@@ -27,7 +37,7 @@ setup(
             [
                 "data/Brain_Lineage_Landmarks_EMtoEM_ProjectSpace.csv",
                 "data/example_project_info.json",
-                "smat_jefferis.csv",
+                "data/smat_jefferis.csv",
             ],
         )
     ],
